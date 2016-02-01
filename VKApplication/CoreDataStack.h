dbaa40e2877b905d58a78264504b672b2bd76ad4
vasylus <vasiji95@gmail.com>
2016-02-01 14:40:54 +0200

    Update CoreDataStack.h

--- VKApplication/CoreDataStack.h
+++ VKApplication/CoreDataStack.h
@@ -2,16 +2,11 @@
 //  CoreDataStack.h
 //  VKApplication
 //
-<<<<<<< HEAD
 //  Created by Vasyl Vasylchenko on 25.01.16.
-=======
-//  Created by Alex on 1/27/16.
->>>>>>> origin/master
 //  Copyright © 2016 Vasyl Vasylchenko. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
-<<<<<<< HEAD
 #import <CoreData/CoreData.h>
 
 
@@ -25,27 +20,3 @@
 - (NSURL *)applicationDocumentsDirectory;
 
 @end
-=======
-@class NSManagedObjectModel;
-@class NSPersistentStoreCoordinator;
-@class NSManagedObjectContext;
-
-@interface CoreDataStack : NSObject
-
-@property (atomic, strong, readonly) NSManagedObjectModel *objectModel;
-@property (atomic, strong, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
-@property (atomic, strong, readonly) NSManagedObjectContext *managedContext;
-
-//This ensures thread-safety for CoreData
-//Use this instead of init
-+ (instancetype)sharedStack;
-
-//Creates and returns a new managed object context that is the child of managedContext
-- (NSManagedObjectContext *)childManagedContext;
-
-//Persists managedObjectContext to disk
-- (void)saveContext;
-
-@end
-
->>>>>>> origin/master
